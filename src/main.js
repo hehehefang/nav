c8e2ec5c9c4989b0eca0c138b12cea79d851b674
Fang <771492072@qq.com>
2021-06-25 19:58:31 +0800

    添加保存

--- src/main.js
+++ src/main.js
@@ -1,12 +1,36 @@
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.last')//在sitelist里面找到last
+const hasMap = [
+    { logo: 'A', url: 'https://www.acfun.cn' },
+    { logo: 'B', url: 'https://www.bilibili.com' }
+]
+
+
+hasMap.forEach((node) => {
+
+    const $li = $(`<li>  
+    <a href="${node.url}">
+        <div class="site">
+            <div class="logo">${node.logo}</div>
+            <div class="link">${node.url}</div>
+        </div>
+    </a>
+    </li>`).insertBefore($lastLi)
+})
+
+
 $('.addButton').on('click', () => {
     let url = window.prompt('请输入url')
     if (url.indexOf('http') !== 0) {
         url = 'https://' + url
         console.log(url)
     }
-    const $siteList = $('.siteList')//找到sitelist
-    console.log($siteList)
-    const $lastLi = $siteList.find('li.last')//在sitelist里面找到last
+
+    hasMap.push({
+        logo: 'url[0]',
+        url: url
+    })
+
     //把li 插到  lastLi  前面
     const $li = $(`<li>  
     <a href="${url}">
@@ -17,3 +41,4 @@ $('.addButton').on('click', () => {
     </a>
     </li>`).insertBefore($lastLi)
 })
+//点网站 进去回来就没了  怎么办
